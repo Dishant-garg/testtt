83371a65a47426f69a582678282498e7783ade9b
vikasiiitl <vikasiet49@gmail.com>
2025-02-20 15:54:38 +0530

    Minor change

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 mkdir -p ~/.nltk_data
 python -c "import nltk; nltk.download('punkt', download_dir='~/.nltk_data'); nltk.download('stopwords', download_dir='~/.nltk_data')"
+python -c "import nltk; nltk.download('punkt_tab', download_dir='~/.nltk_data'); nltk.download('stopwords', download_dir='~/.nltk_dataa')"
